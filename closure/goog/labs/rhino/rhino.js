aa21dd44c10a309a2f2b78a346a9bde1fc8cff21
Nathan Naze <nanaze@gmail.com>
2014-04-13 13:41:17 -0400

    update rhino check since raw rhino does not expose environment var

--- closure/goog/labs/rhino/rhino.js
+++ closure/goog/labs/rhino/rhino.js
@@ -1,9 +1,10 @@
 goog.provide('goog.labs.rhino');
 
+goog.require('goog.functions');
+
 /**
  * @return {boolean} Whether this is Rhino.
  */ 
-goog.labs.rhino.isRhino = function() {
-    var env = goog.global['environment'];
-    return env && (!!env['java.version']);
-}
+goog.labs.rhino.isRhino = goog.functions.cacheReturnValue(function() {
+    return ("" + java) == "[JavaPackage java]";
+});
